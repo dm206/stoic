4ac280e09ae60759907fdbb6ac2ebfe95073b812
David Marks <davidmarks206@gmail.com>
2026-02-03 18:15:05 -0800

    make my page html

--- index.php
+++ index.php
@@ -37,26 +37,6 @@ $alive = $today - strtotime($bday);
 include('elements/footer.php');
 ?>
 
-<script type="importmap">
-  {
-    "imports": {
-      "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
-    }
-  }
-</script>
 
-<div id="app">{{ message }}</div>
-
-<script type="module">
-  import { createApp } from 'vue'
-
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
-      }
-    }
-  }).mount('#app')
-</script>
 </body>
 </html>
